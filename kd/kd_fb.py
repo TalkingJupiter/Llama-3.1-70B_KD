2889d49e248c45e36a6ed109902b22fdbac74de2
Batuhan Sencer <batuhan.sencer@ttu.edu>
2025-09-04 15:38:24 -0500

    Added the forgoten parameters into __init__ func

--- kd/kd_fb.py
+++ kd/kd_fb.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class LinearProjector(nn.Module):
-    def __init__():
+    def __init__(self, in_dim: int, out_dim: int):
         super().__init__()
         self.proj = nn.Linear(in_dim, out_dim, bias=False)
     def forward(self, x):
